3c9573f6200ccce51495438dda632ab7983251cd
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-12-29 03:30:16 +0300

    add models course and lesson

--- users/models.py
+++ users/models.py
@@ -17,3 +17,6 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f'{self.email}'
