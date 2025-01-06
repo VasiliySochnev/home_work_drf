b61b37d9e41ea3ae6172fc680b3161b78b4f2cc1
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2025-01-06 04:11:25 +0300

    adding model Payments

--- users/models.py
+++ users/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course
+
 
 class User(AbstractUser):
+    """Модель пользователя."""
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
     first_name = models.CharField(max_length=50, verbose_name="Имя")
@@ -24,3 +27,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.email}"
+
+
+class Payments(models.Model):
+    """Модель платежи."""
+
+    CASH = "Наличные"
+    TRANSFER = "Перевод на счет"
+
+    WAY_PAY_CHOICES = [
+        (CASH, "Наличные"),
+        (TRANSFER, "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', blank=True, null=True, related_name='user')
+    date_pay = models.DateTimeField(verbose_name='Дата платежа', blank=True, null=True)
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс', blank=True, null=True, related_name='paid_course')
+    amount_pay = models.PositiveIntegerField(verbose_name='Сумма оплаты', blank=True, null=True)
+    way_pay = models.CharField(max_length=50, choices=WAY_PAY_CHOICES, verbose_name='Способ оплаты', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f"{self.amount_pay}"
