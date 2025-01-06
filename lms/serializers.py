fc83ee814d5374ae2a1011f6dc62e0b7d1238d41
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2025-01-06 03:35:03 +0300

    add in serializer for course serializer for quantity lessons

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,15 +3,21 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    lesson_count = serializers.SerializerMethodField()
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+    def get_lesson_count(self, instans):
+        if instans.lessons.all().count():
+            return instans.lessons.all().count()
+        return 0
+
