1e6b625a48a415a887a0c56b9db158ee2893f19e
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2025-02-06 12:45:48 +0300

    add celery, celery-beat, .env.sample

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework_simplejwt",
     "drf_yasg",
+    "django_celery_beat",
 ]
 
 
@@ -132,3 +133,34 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+CUR_API_URL = 'https://api.currencyapi.com/'
+
+CUR_API_KEY = os.getenv('CUR_API_KEY')
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+
+CELERY_TIMEZONE = TIME_ZONE
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+# В этой версии параметр broker_connection_retry
+# больше не управляет попытками повторного подключения к брокеру
+# во время запуска. Вместо этого вам нужно использовать новый параметр broker_connection_retry_on_startup.
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
